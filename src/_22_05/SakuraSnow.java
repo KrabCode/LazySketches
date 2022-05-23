884081da40de59d71100c8dac4014423b9f570e9
Jakub Rak <rak@fss.eu>
2022-05-23 10:50:47 +0200

    improved radial particle mirroring

--- src/_22_05/SakuraSnow.java
+++ src/_22_05/SakuraSnow.java
@@ -21,7 +21,7 @@ public class SakuraSnow extends PApplet {
 
     @Override
     public void settings() {
-        size(1080,1080,P2D);
+        size(1000,1000,P2D);
     }
 
     @Override
@@ -31,26 +31,28 @@ public class SakuraSnow extends PApplet {
 
     }
 
+    int recFrame = 0;
+    boolean isRec = false;
+
     @Override
     public void draw() {
         pg.colorMode(HSB,1,1,1,1);
         pg.beginDraw();
         drawMainBackground();
         pg.clear();
+        pg.translate(width/2f, height/2f);
         updateLeaves();
         pg.endDraw();
-        pushMatrix();
-        translate(width/2f, height/2f);
-        imageMode(CENTER);
-        int mirrorCount = gui.sliderInt("mirrors", 1);
-        for (int mirrorIndex = 0; mirrorIndex < mirrorCount; mirrorIndex++) {
-            pushMatrix();
-            rotate(TAU*norm(mirrorIndex, 0, mirrorCount));
-
-            image(pg, 0, 0);
-            popMatrix();
+        int recSize = gui.sliderInt("record/size", 600);
+        if(gui.toggle("record/active", false)){
+            isRec = true;
+            pg.get(width/2 - recSize / 2, height / 2 - recSize / 2, recSize, recSize).save("/out/sakura/" + recFrame++ + ".jpg");
+        }else{
+            isRec = false;
+            recFrame = 1;
         }
-        popMatrix();
+        imageMode(CORNER);
+        image(pg, 0, 0, width, height);
         gui.themePicker();
         gui.draw();
     }
@@ -64,6 +66,11 @@ public class SakuraSnow extends PApplet {
             }
         }
         pg.rectMode(CENTER);
+        if("blend".equals(gui.stringPicker("leaf/blend mode", new String[]{"blend", "add"}))){
+            pg.blendMode(BLEND);
+        }else{
+            pg.blendMode(ADD);
+        }
         for(Leaf leaf : leaves){
             leaf.update();
             if(leaf.isGarbage()){
@@ -85,24 +92,26 @@ public class SakuraSnow extends PApplet {
         PVector pos = new PVector(), spd = new PVector();
         float timePos;
         int frameBorn = frameCount;
-        float randomConstant = random(1);
         float sizeModifier = randomGaussian() * gui.slider("leaf/draw/size variation", 1);
         private int lifeLength;
 
         Leaf(){
+            float x = gui.slider("leaf/spawn/x", 0);
+            float y = gui.slider("leaf/spawn/y", 0);
             float spawnRangeX = randomGaussian() * gui.slider("leaf/spawn/range x");
             float spawnRangeY = randomGaussian() *  gui.slider("leaf/spawn/range y");
-            pos.x = gui.slider("leaf/spawn/x", width/2f) + spawnRangeX;
-            pos.y =  gui.slider("leaf/spawn/y", height/2f) + spawnRangeY;
+            pos.x = x + spawnRangeX;
+            pos.y = y + spawnRangeY;
 
         }
 
         void update(){
             PVector acc = new PVector(gui.slider("leaf/move/acc x"), gui.slider("leaf/move/acc y"));
+            lifeLength = gui.sliderInt("leaf/spawn/lifetime", 120);
             timePos += radians(gui.slider("leaf/move/noise/time", 1));
             float freq = gui.slider("leaf/move/noise/freq", 0.1f);
             PVector noise = new PVector(
-                    noise(pos.x * freq + randomConstant, pos.y* freq  + randomConstant, timePos),
+                    noise(pos.x * freq, pos.y* freq, timePos),
                     noise(pos.x * freq +430.95f, pos.y* freq -1740.125f, timePos+320.5f)
             );
             noise.sub(0.5f, 0.5f);
@@ -117,22 +126,25 @@ public class SakuraSnow extends PApplet {
             if(frameCount >= frameBorn + lifeLength - fadeOutDuration){
                 fade =  1 - constrain(norm(frameCount, frameBorn + lifeLength - fadeOutDuration, frameBorn + lifeLength), 0, 1);
             }
-            pg.pushMatrix();
-            pg.translate(pos.x, pos.y);
-            pg.rotate(spd.heading());
-            pg.fill(baseFill.hue, baseFill.saturation, baseFill.brightness, lerp(0, baseFill.alpha, fade));
-            pg.noStroke();
-            float size = gui.slider("leaf/draw/size", 2) + sizeModifier;
-            if(gui.stringPicker("leaf/shape", new String[]{"rectangle", "circle"}).equals("rectangle")){
-                pg.rect(0,0,size,size);
-            }else{
-                pg.ellipse(0,0,size,size);
+            int mirrorCount = gui.sliderInt("mirrors", 1);
+            for (int mirrorIndex = 0; mirrorIndex < mirrorCount; mirrorIndex++) {
+                pg.pushMatrix();
+                pg.rotate(TAU * norm(mirrorIndex, 0, mirrorCount));
+                pg.translate(pos.x, pos.y);
+                pg.rotate(spd.heading());
+                pg.fill(baseFill.hue, baseFill.saturation, baseFill.brightness, lerp(0, baseFill.alpha, fade));
+                pg.noStroke();
+                float size = gui.slider("leaf/draw/size", 2) + sizeModifier;
+                if (gui.stringPicker("leaf/shape", new String[]{"rectangle", "circle"}).equals("rectangle")) {
+                    pg.rect(0, 0, size, size);
+                } else {
+                    pg.ellipse(0, 0, size, size);
+                }
+                pg.popMatrix();
             }
-            pg.popMatrix();
         }
 
         public boolean isGarbage() {
-            lifeLength = gui.sliderInt("leaf/spawn/lifetime", 120);
             return frameBorn + lifeLength < frameCount;
         }
     }
