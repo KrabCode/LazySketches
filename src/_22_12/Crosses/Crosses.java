f17fd224263dd87e43dee7d33e494ea0c95beac9
Jakub Rak <rak@fss.eu>
2022-12-12 10:54:19 +0100

    cooler crosses much improved

--- src/_22_12/Crosses/Crosses.java
+++ src/_22_12/Crosses/Crosses.java
@@ -46,17 +46,21 @@ public class Crosses extends PApplet {
         pg.rectMode(CENTER);
         gui.pushFolder("crosses");
         pg.translate(width/2f, height/2f);
-        int count = gui.sliderInt("x count", 12, 0, Integer.MAX_VALUE);
-        float sideShort = gui.slider("rect size", 30);
+        int count = gui.sliderInt("count", 20, 0, Integer.MAX_VALUE);
+        float sideShort = gui.slider("rect size", 10);
         float sideLong = sideShort * 3;
         PVector fieldPos = gui.plotXY("field pos");
-        pg.translate(fieldPos.x, fieldPos.y);
         pg.stroke(gui.colorPicker("stroke").hex);
         pg.strokeWeight(gui.slider("weight", 1.75f));
-        pg.fill(gui.colorPicker("fill").hex);
-        PVector step = gui.plotXY("step", sideShort*4, sideShort * 2);
-        PVector stepDown = step.copy().rotate(-HALF_PI);
+        pg.fill(gui.colorPicker("fill", color(0xFFB0B0B0)).hex);
+        PVector stepRight = gui.plotXY("step", 1, 1);
+        stepRight.x *= sideShort*4;
+        stepRight.y *= sideShort*2;
+        PVector stepDown = stepRight.copy().rotate(-HALF_PI);
         PVector colPos = new PVector();
+        fieldPos.add(PVector.mult(stepRight, -count/2f));
+        fieldPos.add(PVector.mult(stepDown, -count/2f));
+        pg.translate(fieldPos.x, fieldPos.y);
         for (int xi = 0; xi < count; xi++) {
             PVector rowPos = colPos.copy();
             for(int yi = 0; yi < count; yi++){
@@ -79,7 +83,7 @@ public class Crosses extends PApplet {
                 pg.popMatrix();
                 rowPos.add(stepDown);
             }
-            colPos.add(step);
+            colPos.add(stepRight);
         }
         gui.popFolder();
         pg.popMatrix();
