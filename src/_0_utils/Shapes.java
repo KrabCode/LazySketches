eff166b2a854ea50946fe96b11ad554e03c5afb2
Krab <jakubpodtrzitkorak@gmail.com>
2023-12-31 20:21:35 +0100

    sinewaves work

--- src/_0_utils/Shapes.java
+++ src/_0_utils/Shapes.java
@@ -12,6 +12,9 @@ import static com.krab.lazy.stores.GlobalReferences.app;
 
 @SuppressWarnings("SuspiciousNameCombination")
 public class Shapes extends PApplet {
+    private static float maxShapeCount = 10;
+    static int maxCountPyramids = 10;
+
     LazyGui gui;
     PGraphics pg;
     private static float gridPosX, gridPosY;
@@ -33,7 +36,7 @@ public class Shapes extends PApplet {
     public void setup() {
         gui = new LazyGui(this);
         pg = createGraphics(width, height, P3D);
-        pg.colorMode(HSB,1,1,1,1);
+        pg.colorMode(HSB, 1, 1, 1, 1);
     }
 
     @Override
@@ -53,33 +56,33 @@ public class Shapes extends PApplet {
 
     public static void drawMovingString(String path, LazyGui gui, PGraphics pg) {
         pg.pushMatrix();
-        String content = gui.text( path + "content", "...");
+        String content = gui.text(path + "content", "...");
         int count = content.length();
-        float time = gui.slider( path + "time");
-        float speed = radians(gui.slider( path + "speed"));
-        gui.sliderSet( path + "time", time + speed);
-        float x = pg.width / 2f + gui.slider( path + "x");
-        float y = pg.height / 2f + gui.slider( path + "y");
-        float w = gui.slider( path + "width", 300);
-        float h = gui.slider( path + "text size", 256);
-        if(gui.toggle( path + "display rect")){
+        float time = gui.slider(path + "time");
+        float speed = radians(gui.slider(path + "speed"));
+        gui.sliderSet(path + "time", time + speed);
+        float x = pg.width / 2f + gui.slider(path + "x");
+        float y = pg.height / 2f + gui.slider(path + "y");
+        float w = gui.slider(path + "width", 300);
+        float h = gui.slider(path + "text size", 256);
+        if (gui.toggle(path + "display rect")) {
             pg.stroke(0xFFFFFFFF);
             pg.strokeWeight(4);
             pg.rectMode(CORNER);
-            pg.rect(x,y,w,h);
+            pg.rect(x, y, w, h);
         }
         pg.textSize(h);
-        pg.translate(0, 0, gui.slider( path + "z"));
-        float sx = gui.slider( path + "shadow off x");
-        float sy = gui.slider( path + "shadow off y");
+        pg.translate(0, 0, gui.slider(path + "z"));
+        float sx = gui.slider(path + "shadow off x");
+        float sy = gui.slider(path + "shadow off y");
         for (int i = 0; i < count; i++) {
             pg.pushMatrix();
             float iNorm = norm(i, 0, count);
             String letter = getLetter(content, i);
             float xOff = ((iNorm + time) % 1f) * w;
-            pg.fill(gui.colorPicker( path + "shadow fill").hex);
+            pg.fill(gui.colorPicker(path + "shadow fill").hex);
             pg.text(letter, x + xOff + sx, y + sy);
-            pg.fill(gui.colorPicker( path + "normal fill").hex);
+            pg.fill(gui.colorPicker(path + "normal fill").hex);
             pg.text(letter, x + xOff, y);
             pg.popMatrix();
         }
@@ -88,30 +91,30 @@ public class Shapes extends PApplet {
 
     public static void drawSimpleText(String path, LazyGui gui, PGraphics pg) {
         gui.pushFolder(path);
-        String content = gui.text( "content");
+        String content = gui.text("content");
         PVector pos = gui.plotXY("pos");
         float rotation = TAU * gui.slider("rotation * tau");
         float x = pg.width / 2f + pos.x;
         float y = pg.height / 2f + pos.y;
-        float textSize = gui.slider( "text size", 256);
+        float textSize = gui.slider("text size", 256);
         String fontPath = gui.text("font path");
         String fontPathWithSize = fontPath + "|" + floor(textSize);
-        if(gui.button("update font")){
+        if (gui.button("update font")) {
             PFont font = app.createFont(fontPath, textSize);
             fontsByPath.put(fontPathWithSize, font);
         }
-        if(fontsByPath.containsKey(fontPathWithSize)){
+        if (fontsByPath.containsKey(fontPathWithSize)) {
             pg.textFont(fontsByPath.get(fontPathWithSize));
         }
         pg.textSize(textSize);
         pg.textAlign(CENTER, CENTER);
         pg.pushMatrix();
-        pg.translate(x,y);
+        pg.translate(x, y);
         pg.rotate(rotation);
-        PVector shadowOffset = gui.plotXY( "shadow offset");
-        pg.fill(gui.colorPicker( "shadow fill", 0xFF000000).hex);
+        PVector shadowOffset = gui.plotXY("shadow offset");
+        pg.fill(gui.colorPicker("shadow fill", 0xFF000000).hex);
         pg.text(content, shadowOffset.x, shadowOffset.y);
-        pg.fill(gui.colorPicker( "normal fill", 0xFFFFFFFF).hex);
+        pg.fill(gui.colorPicker("normal fill", 0xFFFFFFFF).hex);
         pg.text(content, 0, 0);
         pg.popMatrix();
         gui.popFolder();
@@ -125,23 +128,30 @@ public class Shapes extends PApplet {
         gui.pushFolder(path);
         pg.pushStyle();
         pg.pushMatrix();
-        pg.translate(pg.width/2f, pg.height/2f);
+        pg.translate(pg.width / 2f, pg.height / 2f);
         gui.pushFolder("Δ global transform");
         translate(gui, pg);
         rotateXYZ(gui, pg);
         gui.popFolder();
         pg.rectMode(CENTER);
         int count = gui.sliderInt("count", 1);
-        for (int i = 0; i < count; i++) {
+        maxCountPyramids = max(maxCountPyramids, count);
+        for (int i = 0; i < maxCountPyramids; i++) {
+            gui.pushFolder("pyramids[" + i + "]");
+            if (i >= count) {
+                gui.hideCurrentFolder();
+                gui.popFolder();
+                continue;
+            } else {
+                gui.showCurrentFolder();
+            }
             pg.pushStyle();
             pg.pushMatrix();
-
-            gui.pushFolder("pyramids[" + i + "]");
             float topHeight = gui.slider("top ■ height", 80);
             float footSize = gui.slider("bot ■ size", 100);
             float cutoffSize = gui.slider("top ■ size", 30);
-            pg.stroke(gui.colorPicker( "stroke", pg.color(255)).hex);
-            pg.strokeWeight(gui.slider( "weight", 5));
+            pg.stroke(gui.colorPicker("stroke", pg.color(255)).hex);
+            pg.strokeWeight(gui.slider("weight", 5));
             pg.fill(gui.colorPicker("fill").hex);
             boolean triangleFanMode = gui.toggle("triangle fan | quads");
             gui.pushFolder("Δ[" + i + "].transform");
@@ -151,9 +161,9 @@ public class Shapes extends PApplet {
             rotateXYZ(gui, pg);
             pg.scale(gui.slider("scale x", 1), gui.slider("scale y", 1));
 
-            if(triangleFanMode){
+            if (triangleFanMode) {
                 pg.beginShape(PConstants.TRIANGLE_FAN);
-                pg.vertex(0,0,topHeight);
+                pg.vertex(0, 0, topHeight);
                 pg.vertex(footSize, -footSize);
                 pg.vertex(-footSize, -footSize);
                 pg.vertex(-footSize, footSize);
@@ -161,7 +171,7 @@ public class Shapes extends PApplet {
                 pg.vertex(footSize, -footSize);
                 pg.endShape();
                 gui.popFolder();
-            }else{
+            } else {
                 pg.beginShape(PConstants.QUAD_STRIP);
                 pg.vertex(cutoffSize, -cutoffSize, topHeight);
                 pg.vertex(footSize, -footSize, 0);
@@ -172,7 +182,7 @@ public class Shapes extends PApplet {
                 pg.vertex(-cutoffSize, cutoffSize, topHeight);
                 pg.vertex(-footSize, footSize, 0);
 
-                pg.vertex(cutoffSize, cutoffSize,  topHeight);
+                pg.vertex(cutoffSize, cutoffSize, topHeight);
                 pg.vertex(footSize, footSize, 0);
 
                 pg.vertex(cutoffSize, -cutoffSize, topHeight);
@@ -185,16 +195,16 @@ public class Shapes extends PApplet {
             gui.pushFolder("bot ■[" + i + "].transform");
             translate(gui, pg);
             rotateXYZ(gui, pg);
-            pg.rect(0, 0, footSize*2, footSize*2);
+            pg.rect(0, 0, footSize * 2, footSize * 2);
             gui.popFolder();
 
-            if(!triangleFanMode){
+            if (!triangleFanMode) {
 
                 gui.pushFolder("top ■[" + i + "].transform");
-                pg.translate(0,0,topHeight);
+                pg.translate(0, 0, topHeight);
                 translate(gui, pg);
                 rotateXYZ(gui, pg);
-                pg.rect(0, 0, cutoffSize*2, cutoffSize*2);
+                pg.rect(0, 0, cutoffSize * 2, cutoffSize * 2);
                 gui.popFolder();
             }
 
@@ -207,7 +217,7 @@ public class Shapes extends PApplet {
         pg.popMatrix();
     }
 
-    private static void translate(LazyGui gui, PGraphics pg){
+    private static void translate(LazyGui gui, PGraphics pg) {
         pg.translate(gui.slider("pos x"), gui.slider("pos y"), gui.slider("pos z"));
     }
 
@@ -221,53 +231,69 @@ public class Shapes extends PApplet {
     }
 
     public static void drawSinewaves(String path, LazyGui gui, PGraphics pg) {
-        int count = gui.sliderInt(path + "count", 1);
-        if (gui.button(path + "add new")) {
-            gui.sliderAdd(path + "count", 1);
+        gui.pushFolder(path);
+        int count = gui.sliderInt("count", 1);
+        if (gui.button("add new")) {
+            gui.sliderAdd("count", 1);
         }
         for (int i = 0; i < count; i++) {
-            String iPath = path + "sines[" + i + "]/";
+            gui.pushFolder("sines[" + i + "]/");
             pg.pushMatrix();
-            int detail = gui.sliderInt( iPath + "detail", 100);
-            float freq = gui.slider( iPath + "freq", 0.01f);
-            float sineTime = gui.slider( iPath + "time", 0);
-            float sineTimeDelta = radians(gui.slider( iPath + "time +", 1));
-            gui.sliderSet( iPath + "time", sineTime + sineTimeDelta);
-            pg.translate(pg.width / 2f + gui.slider( iPath + "x"), pg.height / 2f + +gui.slider( iPath + "y"));
-            float w = gui.slider( iPath + "width", 400);
-            float h = gui.slider( iPath + "height", 200);
+            int detail = gui.sliderInt("detail", 100);
+            float freq = gui.slider("freq", 0.01f);
+            float sineTime = gui.slider("time", 0);
+            float sineTimeDelta = radians(gui.slider("time +", 1));
+            gui.sliderSet("time", sineTime + sineTimeDelta);
+            PVector pos = gui.plotXY("pos");
+            pg.translate(pg.width / 2f + pos.x, pg.height / 2f + pos.y);
+            PVector size = gui.plotXY("size", 400, 100);
+            pg.rotate(gui.slider("rotate"));
+            float w = size.x;
+            float h = size.y;
             pg.noFill();
-            pg.stroke(gui.colorPicker( iPath + "stroke", pg.color(255)).hex);
-            pg.strokeWeight(gui.slider( iPath + "weight", 5));
-            float z = gui.slider( iPath + "pos z");
+            pg.stroke(gui.colorPicker("stroke", pg.color(255)).hex);
+            pg.strokeWeight(gui.slider("weight", 5, 0, app.height));
+//            float z = gui.slider("pos z");
             pg.beginShape();
             for (int j = 0; j < detail; j++) {
                 float norm = norm(j, 0, detail - 1);
                 float x = -w / 2f + w * norm;
                 float y = h * sin(norm * freq * TAU + sineTime);
 
-                pg.vertex(x, y, z);
+                pg.vertex(x, y); // , z);
             }
             pg.endShape();
             pg.popMatrix();
+            gui.popFolder();
         }
+        gui.popFolder();
     }
 
     public static void drawShapes(String path, LazyGui gui, PGraphics pg) {
-        if(!path.endsWith("/")){
+        if (!path.endsWith("/")) {
             path += "/";
         }
         int count = gui.sliderInt(path + "count", 1);
         if (gui.button(path + "add new")) {
             gui.sliderAdd(path + "count", 1);
         }
-        for (int i = 0; i < count; i++) {
+
+        maxShapeCount = max(count, maxShapeCount);
+        for (int i = 0; i < maxShapeCount; i++) {
+            String iPath = path + "shapes[" + i + "]/";
+            gui.pushFolder(iPath);
+            if (i >= count) {
+                gui.hideCurrentFolder();
+                gui.popFolder();
+                continue;
+            } else {
+                gui.showCurrentFolder();
+            }
             pg.pushMatrix();
             pg.pushStyle();
-            String iPath = path + "shapes[" + i + "]/";
-            boolean isRect = gui.toggle( iPath + "rect | ellipse", false);
-            boolean center = gui.toggle( iPath + "corner | center", true);
-            boolean cutout = gui.toggle( iPath + "normal | cutout", false);
+            boolean isRect = gui.toggle("rect | ellipse", false);
+            boolean center = gui.toggle("corner | center", true);
+            boolean cutout = gui.toggle("normal | cutout", false);
             if (center) {
                 pg.rectMode(CENTER);
                 pg.ellipseMode(CENTER);
@@ -275,25 +301,25 @@ public class Shapes extends PApplet {
                 pg.rectMode(CORNER);
                 pg.ellipseMode(CORNER);
             }
-            pg.stroke(gui.colorPicker( iPath + "stroke", 0xFF000000).hex);
-            if (gui.toggle( iPath + "no stroke")) {
+            pg.stroke(gui.colorPicker("stroke", 0xFF000000).hex);
+            if (gui.toggle("no stroke")) {
                 pg.noStroke();
             }
-            pg.strokeWeight(gui.slider( iPath + "weight", 1));
-            pg.fill(gui.colorPicker( iPath + "fill", 0xFF202020).hex);
-            if (gui.toggle( iPath + "no fill")) {
+            pg.strokeWeight(gui.slider("weight", 1));
+            pg.fill(gui.colorPicker("fill", 0xFF202020).hex);
+            if (gui.toggle("no fill")) {
                 pg.noFill();
             }
-            float x = pg.width / 2f + gui.slider( iPath + "pos x");
-            float y = pg.height / 2f + gui.slider( iPath + "pos y");
-            float z = gui.slider( iPath + "pos z", 1);
-            float w = gui.slider( iPath + "size x", 100);
-            float h = gui.slider( iPath + "size y", 100);
+            float x = pg.width / 2f + gui.slider("pos x");
+            float y = pg.height / 2f + gui.slider("pos y");
+            float z = gui.slider("pos z", 1);
+            float w = gui.slider("size x", 100);
+            float h = gui.slider("size y", 100);
             pg.translate(x, y, z);
-            pg.rotate(gui.slider( iPath + "rotate"));
-            gui.sliderAdd( iPath + "rotate", radians(gui.slider( iPath + "rotate +")));
-            pg.translate(gui.slider( iPath + "pos x 2"), gui.slider( iPath + "pos y 2"));
-            pg.scale(gui.slider( iPath + "scale x", 1), gui.slider( iPath + "scale y", 1));
+            pg.rotate(gui.slider("rotate"));
+            gui.sliderAdd("rotate", radians(gui.slider("rotate +")));
+            pg.translate(gui.slider("pos x 2"), gui.slider("pos y 2"));
+            pg.scale(gui.slider("scale x", 1), gui.slider("scale y", 1));
             if (!cutout) {
                 if (isRect) {
                     pg.rect(0, 0, w, h);
@@ -325,6 +351,7 @@ public class Shapes extends PApplet {
             }
             pg.popStyle();
             pg.popMatrix();
+            gui.popFolder();
         }
     }
 
@@ -374,18 +401,18 @@ public class Shapes extends PApplet {
         pg.pushStyle();
         pg.noStroke();
         pg.rectMode(CORNER);
-        if(gui.toggle("nice fade", false)){
+        if (gui.toggle("nice fade", false)) {
             int subEveryNFrames = gui.sliderInt("subtract skip frames", 3);
             PickerColor sub = gui.colorPicker("nice subtract", pg.color(0xFF010101));
             PickerColor min = gui.colorPicker("nice minimum", pg.color(0xFF232323));
-            if(subEveryNFrames != 0 && backgroundFrameCount++ % subEveryNFrames == 0){
+            if (subEveryNFrames != 0 && backgroundFrameCount++ % subEveryNFrames == 0) {
                 pg.blendMode(PConstants.SUBTRACT);
                 pg.fill(sub.hex);
                 pg.rect(0, 0, pg.width, pg.height);
             }
             pg.blendMode(PConstants.LIGHTEST);
             pg.fill(min.hex);
-        }else{
+        } else {
             pg.fill(bg.hex);
         }
         pg.rect(0, 0, pg.width, pg.height);
--- src/_23_12/TextMask.java
+++ src/_23_12/TextMask.java
@@ -1,25 +1,29 @@
 package _23_12;
 
+import _0_utils.Shapes;
 import _0_utils.Utils;
-import com.krab.lazy.ShaderReloader;
+import com.krab.lazy.*;
 import processing.core.PApplet;
 import processing.core.PGraphics;
-import com.krab.lazy.LazyGui;
 import processing.core.PVector;
 
-public class TextMask extends PApplet {
-    LazyGui gui;
-    PGraphics bg;
-    PGraphics fg;
-    private float backgroundTime;
+import java.util.HashMap;
 
+public class TextMask extends PApplet {
+    private LazyGui gui;
+    private PGraphics bg;
+    private PGraphics tg;
+    private float backgroundTime;
+    private HashMap<String, Integer> xAligns;
+    private HashMap<String, Integer> yAligns;
     public static void main(String[] args) {
         PApplet.main(java.lang.invoke.MethodHandles.lookup().lookupClass());
     }
 
     @Override
     public void settings() {
-        size(1080, 1080, P2D);
+        fullScreen(P3D, 2);
+//        size(1080, 1080, P2D);
         smooth(8);
     }
 
@@ -27,14 +31,8 @@ public class TextMask extends PApplet {
     public void setup() {
         gui = new LazyGui(this);
         colorMode(HSB, 1, 1, 1, 1);
-        bg = createGraphics(width, height, P2D);
-        bg.beginDraw();
-        bg.colorMode(HSB, 1, 1, 1, 1);
-        bg.endDraw();
-        fg = createGraphics(width, height);
-        fg.beginDraw();
-        fg.colorMode(HSB, 1, 1, 1, 1);
-        fg.endDraw();
+        bg = createGraphics(width, height, P3D);
+        tg = createGraphics(width, height);
         frameRate(60);
     }
 
@@ -45,10 +43,15 @@ public class TextMask extends PApplet {
         bg.endDraw();
         image(bg, 0, 0);
 
-        fg.beginDraw();
-        drawTextMask(fg);
-        fg.endDraw();
-        image(fg, 0, 0);
+        PickerColor backgroundColor = gui.colorPicker("background", color(0.1f));
+        tg.beginDraw();
+        tg.blendMode(BLEND);
+        tg.background(backgroundColor.hex);
+        tg.blendMode(REPLACE);
+        Shapes.drawSinewaves("sines", gui, tg);
+        drawTextMask(tg);
+        tg.endDraw();
+        image(tg, 0, 0);
 
         Utils.record(this, gui);
     }
@@ -56,32 +59,41 @@ public class TextMask extends PApplet {
     private void drawBackground(PGraphics pg) {
         String shaderPath = "_23_12/text_mask.frag";
         backgroundTime += radians(1) * gui.slider("bg time +=", 1);
+
         ShaderReloader.getShader(shaderPath).set("time", backgroundTime);
+        ShaderReloader.getShader(shaderPath).set("scale", gui.slider("scale", 1));
         ShaderReloader.filter(shaderPath, pg);
     }
 
     private void drawTextMask(PGraphics pg) {
-        gui.pushFolder("text mask");
-        pg.blendMode(BLEND);
-        pg.background(gui.colorPicker("background", color(0.1f)).hex);
-        pg.blendMode(REPLACE);
-        pg.pushMatrix();
-        {
-            PVector textPos = gui.plotXY("pos");
-            int fontSize = gui.sliderInt("size", 32);
-            pg.textFont(Utils.getFont(this,
-                gui.text("font", "Comic Sans MS"),
-                    fontSize
-            ));
-            pg.translate(width / 2f, height / 2f);
-            pg.translate(textPos.x, textPos.y);
-            pg.textAlign(CENTER, CENTER);
-            pg.fill(gui.colorPicker("text color", color(0.9f)).hex);
-            pg.text(gui.text("content", "hello"), 0, 0);
+        if (xAligns == null || yAligns == null) {
+            xAligns = new HashMap<>();
+            xAligns.put("LEFT", LEFT);
+            xAligns.put("CENTER", CENTER);
+            xAligns.put("RIGHT", RIGHT);
+            yAligns = new HashMap<>();
+            yAligns.put("TOP", TOP);
+            yAligns.put("CENTER", CENTER);
+            yAligns.put("BOTTOM", BOTTOM);
         }
+
+        gui.pushFolder("text mask");
+        pg.pushMatrix();
+        PVector textPos = gui.plotXY("pos");
+        int fontSize = gui.sliderInt("size", 32);
+        String fontName = gui.text("font", "Comic Sans MS");
+        pg.textFont(Utils.getFont(this, fontName, fontSize));
+        pg.translate(width / 2f, height / 2f);
+        pg.translate(textPos.x, textPos.y);
+        pg.textLeading(fontSize * gui.slider("leading"));
+        pg.textAlign(
+                xAligns.get(gui.radio("align x", xAligns.keySet().toArray(new String[0]))),
+                yAligns.get(gui.radio("align y", yAligns.keySet().toArray(new String[0])))
+        );
+        pg.fill(gui.colorPicker("text color", color(0.9f)).hex);
+        pg.text(gui.text("content", "hello"), 0, 0);
         pg.popMatrix();
         gui.popFolder();
-
     }
 }
 
